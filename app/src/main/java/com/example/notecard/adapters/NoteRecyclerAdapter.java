6a4d9d23e1828ed95cadf2a9e4c3c765544b3ec0
undencem <undencem@gmail.com>
2021-02-06 06:02:03 +0300

    third tutorial

--- app/src/main/java/com/example/notecard/adapters/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/notecard/adapters/NoteRecyclerAdapter.java
@@ -6,6 +6,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+/**
+ * Instead of ListView , RecyclerView.ViewHolder is recommended
+ */
 public class NoteRecyclerAdapter extends RecyclerView.ViewHolder {
 
     private TextView title,timestamp;
